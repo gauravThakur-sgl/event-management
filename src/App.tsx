10a4f94bf18c3489b6bfeda9474004b0028f290a
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-01-17 17:48:31 +0530

    project initial-setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black text-lime-500 text-center p-2">
+        <h2 className="font-semibold">Event Management</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
