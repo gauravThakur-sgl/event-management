b7b122b17893929e983b6b8b817a114d86a7aafc
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-01-21 15:50:51 +0530

    Fixed footer alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,29 +2,31 @@ export const Footer = () => {
   const listStyle = "flex flex-col  items-start text-gray-400 gap-2";
   return (
     <div className="bg-black text-white">
-      <div className="pt-28 mx-2 md:mx-40">
-        <div className="font-semibold text-3xl text-center md:text-left">Event Management</div>
-        {/* Social Media Icons */}
-        <ul className="flex justify-center lg:justify-end items-center gap-2 pt-5">
-          <li className="bg-white rounded-full p-2 h-9 w-9">
-            <img src="/Twitter.svg" alt="Twitter" className="" />
-          </li>
-          <li className="bg-white rounded-full p-2 h-9 w-9">
-            <img src="/Instagram.svg" alt="Telegram" />
-          </li>
-          <li className="bg-white rounded-full p-2 h-9 w-9">
-            <img src="/Facebook.svg" alt="Facebook" />
-          </li>
-          <li className="bg-white rounded-full p-2 h-9 w-9">
-            <img src="/Instagram.svg" alt="Instagram" />
-          </li>
-        </ul>
-        <div className="text-gray-400 font-medium text-center md:text-left pt-5 mx-2">
+      <div className="pt-10 mx-2 md:mx-40">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="font-semibold text-3xl text-center md:text-left">Event Management</div>
+          {/* Social Media Icons */}
+          <ul className="flex justify-center lg:justify-end items-center gap-2">
+            <li className="bg-white rounded-full p-2 h-9 w-9">
+              <img src="/Twitter.svg" alt="Twitter" className="" />
+            </li>
+            <li className="bg-white rounded-full p-2 h-9 w-9">
+              <img src="/Instagram.svg" alt="Telegram" />
+            </li>
+            <li className="bg-white rounded-full p-2 h-9 w-9">
+              <img src="/Facebook.svg" alt="Facebook" />
+            </li>
+            <li className="bg-white rounded-full p-2 h-9 w-9">
+              <img src="/Instagram.svg" alt="Instagram" />
+            </li>
+          </ul>
+        </div>
+        <div className="text-gray-400 font-medium text-center md:text-left pt-5">
           Creating Unforgettable Experiences, One Event at a Time
         </div>
 
         {/* Footer Links */}
-        <div className="flex flex-col lg:flex-row items-center gap-5 lg:gap-24 xl:gap-32 2xl:gap-56 pt-20">
+        <div className="flex flex-col lg:flex-row gap-5 px-4 md:gap-4 lg:gap-16 xl:gap-32 2xl:gap-56 pt-20">
           <ul className={`${listStyle}`}>
             <li className="text-2xl font-medium text-white">Product</li>
             <li>Features</li>
@@ -55,6 +57,7 @@ export const Footer = () => {
             <li>Terms of Service</li>
             <li>Cookie Policy</li>
             <li>Security</li>
+            <li>GDPR Compliance</li>
           </ul>
           <ul className={`${listStyle}`}>
             <li className="text-2xl font-medium text-white">Social</li>
@@ -62,10 +65,11 @@ export const Footer = () => {
             <li>LinkedIn</li>
             <li>Phone</li>
             <li>Instagram</li>
+            <li>Facebook</li>
           </ul>
         </div>
       </div>
-      <div className="flex flex-col justify-center md:flex-row  md:justify-around items-center gap-2 py-10 border-t border-gray-400 mt-10">
+      <div className="flex flex-col justify-center md:flex-row  md:justify-around items-center gap-2 py-5 border-t border-gray-400 mt-10">
         <div>Terms and Conditions</div>
         <div className="text-center">© 2023 Event Management. All rights reserved.</div>
         <div>Cookies Policy</div>
