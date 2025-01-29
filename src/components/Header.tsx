844455faad24d53b903e15c5d4ca6552e5738fe0
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-01-29 19:23:57 +0530

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "./ui/navigation-menu";
+import { useNavigate } from "react-router-dom";
 interface INavbarProps {
   featureRef: React.RefObject<HTMLDivElement>;
   pricingRef: React.RefObject<HTMLDivElement>;
@@ -25,6 +26,7 @@ export const Header = ({
   eventCalendarRef,
   eventTodoRef,
 }: INavbarProps) => {
+  const navigate = useNavigate();
   const scrollToRef = (ref: React.RefObject<HTMLDivElement>) => {
     if (ref && ref.current) {
       ref.current.scrollIntoView({ behavior: "smooth" });
@@ -72,6 +74,9 @@ export const Header = ({
       subFeatures: [],
     },
   ];
+  const handleLogin = () => {
+    navigate("/login");
+  };
   return (
     <>
       <div className="flex justify-between items-center bg-green-600 h-16 p-2 lg:p-4 md:px-6 xl:px-32 2xl:px-56 fixed top-0 w-full z-20">
@@ -81,7 +86,7 @@ export const Header = ({
           </span>
           {/* Event Management */}
         </div>
-        
+
         <div className="hidden lg:block">
           <NavigationMenu className="flex justify-center items-center gap-4 text-lg text-white bg-green-600">
             <NavigationMenuList>
@@ -124,7 +129,7 @@ export const Header = ({
           <Menu className="md:hidden" />
           <div className="hidden md:block">
             <span className="flex justify-center items-center gap-2">
-              <Button variant="outline" className="border-none text-base rounded-full px-8">
+              <Button variant="outline" className="border-none text-base rounded-full px-8" onClick={handleLogin}>
                 Login
               </Button>
             </span>
